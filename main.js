ffb637f618c121992002ad61a94440017787e89b
visualxerox <visualxerox@gmail.com>
2018-12-04 17:15:23 -0600

    html y css cai listos

---
+++ main.js
@@ -0,0 +1,5 @@
+// console.log ("Hola"); Para validar que este conectado
+var request = new XMLHttpRequest();
+   request.open("GET", "images.json", false);
+   request.send(null)
+   alert (request);
